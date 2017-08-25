1450567f991d03ee19eafd68df38169c72f832e5
ltch23 <luistong23@gmail.com>
2017-08-25 00:46:03 -0500

    ejercicios 1-4

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -1,17 +1,21 @@
 #define GLUT_DISABLE_ATEXIT_HACK	
+#define _USE_MATH_DEFINES
 //#include <windows.h>
 #include <math.h>
 #include <GL/glut.h>
+#include <iostream>
 
+using namespace std;
 
 #define KEY_ESC 27
+int x=0,y=0, len_edge=0;
+
 
 //dibuja un simple gizmo
 void displayGizmo()
 {
 	glBegin(GL_LINES);
 	glColor3d(255,0,0);
-	glVertex2d(0, 0);
 	glVertex2d(20, 0);
 	glColor3d(0, 255, 0);
 	glVertex2d(0, 0);
@@ -32,7 +36,66 @@ void displayGizmo2()
 	glEnd();
 }
 
-//
+//  1) dibuja cuadrado
+void displaySquare(int x,int y, int edge)
+{
+	int tmp=edge*0.5;
+	glBegin(GL_LINE_LOOP);
+	glColor3d(255,0,0);
+	glVertex2d(x-tmp, y+tmp);
+	glVertex2d(x+tmp, y+tmp);
+	glVertex2d(x+tmp, y-tmp);
+	glVertex2d(x-tmp, y-tmp);
+	glEnd();
+	
+}
+
+// 2) dibuja circulo
+void displayCircle(int x,int y,int r)
+{
+	int number_edges=10; //*2 por 2*PI
+	glBegin(GL_LINE_LOOP);
+	for(float i=0; i<2*M_PI; i+= M_PI/number_edges)
+		glVertex2f((r*cos(i))+x, (r*sin(i)+y));
+	glEnd();
+}
+
+
+// 3) dibuja circulos desde un mismo punto izquierda
+void displayCircles(int number_circles,int red_radio){
+	float radio_default=30;
+	int x=0;y=0;
+	displayCircle(x,y,radio_default); 
+	radio_default-=red_radio;
+	
+	for( int i=1; i<number_circles;i++,radio_default-=red_radio){
+		if(radio_default<0) break;
+		x-=red_radio;
+		displayCircle(x,y,radio_default);
+	}
+}	
+
+
+// 4) dibuja circulos de mayor a menor icontinuos 
+void displayCircles2(int number_circles,int red_radio){
+	float radio_default=10;
+	float tmp_radio=radio_default;
+	int x=-40;y=0;
+	displayCircle(x,y,radio_default); 
+	radio_default-=red_radio; // no perder radio para avanzar
+	
+	for( int i=1; i<number_circles;i++,radio_default-=red_radio){
+		if(radio_default<0) break;
+		
+		x+=tmp_radio+radio_default;// suma de radio anterior y actual
+		displayCircle(x,y,radio_default);
+		tmp_radio=radio_default;
+		
+	}
+}	
+
+
+
 //funcion llamada a cada imagen
 void glPaint(void) {
 
@@ -41,8 +104,21 @@ void glPaint(void) {
 	glLoadIdentity();
 	
 	
-	//dibuja el gizmo
-	displayGizmo();
+	// dibuja el gizmo
+	// displayGizmo();
+	
+	// dibuja cuadrado
+	// displaySquare(x,y,len_edge);
+	
+	// dibuja circulo
+	// displayCircle(-30,-15,10);
+	
+	// dibuja circulo
+	// displayCircles(10,4);
+	
+	// dibuja circulo
+	// displayCircles2(7,1);g
+
 
 	//doble buffer, mantener esta instruccion al fin de la funcion
 	glutSwapBuffers();
@@ -86,9 +162,16 @@ GLvoid window_key(unsigned char key, int x, int y) {
 //
 int main(int argc, char** argv) {
 
+	//datos por consola
+
+/* 	cout<<"datos"<<endl;
+	cin>>x>>y>>len_edge;
+ */
+ 
 	//Inicializacion de la GLUT
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(600, 600); //tama�o de la ventana
 	glutInitWindowSize(600, 600); //tama�o de la ventana
 	glutInitWindowPosition(100, 100); //posicion de la ventana
 	glutCreateWindow("TP1 OpenGL : hello_world_OpenGL"); //titulo de la ventana
