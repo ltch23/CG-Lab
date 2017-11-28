72f625d21e8dacc9cd3a571fc958bb23c31f9fc6
ltch23 <luistong23@gmail.com>
2017-11-28 12:24:16 -0500

    final tp4

--- TP4/tp4.cpp
+++ TP4/tp4.cpp
@@ -13,32 +13,23 @@ using namespace std;
 
 #define ECHAP 27
 
-
-void init_scene();
-void draw();
-GLvoid initGL();
-GLvoid window_display();
-GLvoid window_reshape(GLsizei width, GLsizei height);
-GLvoid window_key(unsigned char key, int x, int y);
-
-
 //variables para el gizmo
 float delta_x = -50.0; 
 float delta_y = 5.0;
 float mouse_x, mouse_y;
 float var_x = 0.0;
 float var_z = -100.0;
-float step = 0; //0.0 Posicion inicial. 1.0 Traslacion. 2.0 Primera Rotacion(en y). 3.0 Segunda Rotacion (en x) 4.0 Ultima Rotacion (en z)
-GLint suelo;
-GLint pared1;
-GLint pared2;
-GLint pared3;
-GLint tejado;
-GLint techo;
+GLint suelo, pared1, pared2, pared3, tejado, techo,tronco;
+GLfloat lightPosition[4];
+GLfloat light_axis_x = 150, light_axis_y = 150, light_axis_z = 150;
 
-///////////////////////////////////////////////////////////////////////////////
-//(1)
-///////////////////////////////////////////////////////////////////////////////
+void init_scene();
+void render_scene();
+GLvoid initGL();
+GLvoid window_display();
+GLvoid window_reshape(GLsizei width, GLsizei height);
+GLvoid window_key(unsigned char key, int x, int y);
+
 GLvoid callback_special(int key, int x, int y)
 {
 	switch (key)
@@ -64,11 +55,11 @@ GLvoid callback_special(int key, int x, int y)
 		break;
 
 	case GLUT_KEY_PAGE_UP:
-		step++;
+		light_axis_z+=15;
 		glutPostRedisplay();
 		break;
-	case GLUT_KEY_PAGE_DOWN:
-		step--;
+		case GLUT_KEY_PAGE_DOWN:
+		light_axis_z-=15;
 		glutPostRedisplay();
 		break;
 
@@ -78,9 +69,6 @@ GLvoid callback_special(int key, int x, int y)
 
 
 
-///////////////////////////////////////////////////////////////////////////////
-//(2)
-///////////////////////////////////////////////////////////////////////////////
 GLvoid callback_mouse(int button, int state, int x, int y)
 {
 	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON)
@@ -90,9 +78,6 @@ GLvoid callback_mouse(int button, int state, int x, int y)
 	}
 }
 
-///////////////////////////////////////////////////////////////////////////////
-//(3)
-///////////////////////////////////////////////////////////////////////////////
 GLvoid callback_motion(int x, int y)
 {
 	
@@ -113,12 +98,10 @@ int main(int argc, char **argv)
 
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 
-
 	glutInitWindowSize(800, 800);
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("TP 3 : Transformations 3D Part 2");
 
-	// suelo = TextureManager::Inst()->LoadTexture("pasto.jpg",GL_BGR_EXT,GL_RGB);
 	suelo = TextureManager::Inst()->LoadTexture("pasto.jpg", GL_BGR_EXT, GL_RGB);
 	pared1 = TextureManager::Inst()->LoadTexture("puerta.jpeg", GL_BGR_EXT, GL_RGB);
 	pared2 = TextureManager::Inst()->LoadTexture("paredatras.jpeg", GL_BGR_EXT, GL_RGB);
@@ -129,7 +112,6 @@ int main(int argc, char **argv)
 	initGL();
 	init_scene();
 
-
 	glutDisplayFunc(&window_display);
 	glutReshapeFunc(&window_reshape);
 	
@@ -138,13 +120,10 @@ int main(int argc, char **argv)
 	glutMotionFunc(&callback_motion);
 
 	glutKeyboardFunc(&window_key);
-	//glutKeyboardUpFunc(&window_key_up); //key release events
-	glutSpecialFunc(&callback_special);
-	//glutSpecialUpFunc(&callback_special_up); //key release events
 	
-	//function called on each frame
+	glutSpecialFunc(&callback_special);
+	
 	glutIdleFunc(&window_idle);
-
 	glutMainLoop();
 
 	return 1;
@@ -160,33 +139,12 @@ GLvoid initGL()
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 
-	GLfloat ambient[]={0.3f,0.3f,0.3f,1.0f};
-	GLfloat diffuse[]={0.8f,0.8f,0.8f,1.0f};
-	GLfloat specular[]={1.0f,1.0f,1.0f,1.0f};
-	GLfloat shinn[]={10.f};
-	
-	GLfloat lightAmbient[]={0.3f,0.3f,0.3f,1.0f};
-	GLfloat lightPosition[]={150.0f,150.0f,150.0f,1.0f};
-
-	glMaterialfv(GL_FRONT_AND_BACK,GL_AMBIENT,ambient);
-	glMaterialfv(GL_FRONT_AND_BACK,GL_DIFFUSE,diffuse);
-	glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR,specular);
-	glMaterialfv(GL_FRONT_AND_BACK,GL_SHININESS,shinn);
-	
-	glLightfv(GL_LIGHT0,GL_AMBIENT,lightAmbient);
-	glLightfv(GL_LIGHT0,GL_POSITION,lightPosition);
-
-
-
 	glClearColor(RED, GREEN, BLUE, ALPHA);
 }
 
 
 void Gizmo3D(){
 
-	
-	glBegin(GL_LINES);
-	glColor3f(1.0f, 0.0f, 0.0f);
 	glVertex3f(0.0f, 0.0f, 0.0f);
 	glVertex3f(3.0f, 0.0f, 0.0f);
 	glEnd();
@@ -197,29 +155,43 @@ void Gizmo3D(){
 	glVertex3f(0.0f, 3.0f, 0.0f);
 	glEnd();
 
-
-	glBegin(GL_LINES);
 	glColor3f(0.0f, 0.0f, 1.0f);
 	glVertex3f(0.0f, 0.0f, 0.0f);
 	glVertex3f(0.0f, 0.0f, 3.0f);
 	glEnd();
-
-
 	return;
-
 }
-
-
 
 GLvoid window_display()
 {
+	GLfloat ambient[] = {0.3f, 0.3f, 0.3f, 1.0f};
+	GLfloat diffuse[] = {0.8f, 0.8f, 0.8f, 1.0f};
+	GLfloat specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
+	GLfloat shinn[] = {10.f};
 
+	GLfloat lightAmbient[] = {0.3f, 0.3f, 0.3f, 1.0f};
+	GLfloat lightEspecular[] = {0.5f, 0.5f, 0.5f, 1.0f};
+	GLfloat lightDifusa[] = {0.5f, 0.5f, 0.5f, 1.0f};
+
+	lightPosition[0] = light_axis_x;
+	lightPosition[1] = light_axis_y;
+	lightPosition[2] = light_axis_z;
+	lightPosition[3] = 1.0f;
+
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, shinn);
+	
+	glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightPosition);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, lightEspecular);
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+	
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-
 
 	gluPerspective(45.0f, 1.0f, 0.01f, 100.0f);
 
@@ -227,11 +199,7 @@ GLvoid window_display()
 	glRotatef(delta_x, 0.0, 1.0, 0.0);
 	glRotatef(delta_y, 1.0, 0.0, 0.0);
 
-
-	// Gizmo3D();
-	draw();
-	
-
+	render_scene();
 	glutSwapBuffers();
 
 	glFlush();
@@ -247,8 +215,6 @@ GLvoid window_reshape(GLsizei width, GLsizei height)
 	glMatrixMode(GL_MODELVIEW);
 }
 
-
-
 void init_scene()
 {
 }
@@ -259,43 +225,62 @@ GLvoid window_key(unsigned char key, int x, int y)
 	case ECHAP:
 		exit(1);
 		break;
+	case 99:
+		light_axis_x += 15;
+		glutPostRedisplay();
+		break;
+	case 122:
+		light_axis_x -= 15;
+		glutPostRedisplay();
+		break;
+	case 115:
+		light_axis_y += 15;
+		glutPostRedisplay();
+		break;
+	case 120:
+		light_axis_y -= 15;
+		glutPostRedisplay();
+		break;
 	default:
 		printf("La touche %d non active.\n", key);
 		break;
 	}
 }
 
-
 //function called on each frame
 GLvoid window_idle()
 {
 	glutPostRedisplay();
 }
-void draw(){
+void render_scene(){
 
+	float pos_x=10.0f;
 	// SUELO
-	// glutSolidSphere(10,64,64);
 	glPushMatrix();
 	glBindTexture(GL_TEXTURE_2D, suelo);
 	glBegin(GL_POLYGON);
 	glNormal3f(1,0,0);
 	glTexCoord2f(0.0,0.0);	glVertex3f(0.0f, 0.0f, 0.0f);
 	glTexCoord2f(5.0,0.0);	glVertex3f(0.0f, 0.0f, 40.0f);
-	glTexCoord2f(5.0,5.0);	glVertex3f(40.0f, 0.0f, 40.0f );
-	glTexCoord2f(0.0,5.0);	glVertex3f(40.0f, 0.0f, 00.0f);
+	glTexCoord2f(5.0,5.0);	glVertex3f(40.0f, 0.0f, 40.0f);
+	glTexCoord2f(0.0,5.0);  glVertex3f(40.0f, 0.0f, 00.0f);
 	glEnd();
 	glPopMatrix();
 	
-	// CASA
+	/* CASA */
 	// PAREDES
 	glPushMatrix();
 	glBindTexture(GL_TEXTURE_2D, pared1);
 	glBegin(GL_POLYGON);
 	glNormal3f(1,0,0);
-	glTexCoord2f(0.0,0.0);	glVertex3f(0.0f, 0.0f, 10.0f);
-	glTexCoord2f(1.0,0.0);	glVertex3f(10.0f, 0.0f, 10.0f);
-	glTexCoord2f(1.0,1.0);	glVertex3f(10.0f, 10.0f, 10.0f );
-	glTexCoord2f(0.0,1.0);	glVertex3f(0.0f, 10.0f, 10.0f);
+	glTexCoord2f(0.0,0.0);
+	glVertex3f(0.0f, 0.0f, 10.0f + pos_x);
+	glTexCoord2f(1.0,0.0);	
+	glVertex3f(10.0f, 0.0f, 10.0f+pos_x);
+	glTexCoord2f(1.0,1.0);
+	glVertex3f(10.0f, 10.0f, 10.0f + pos_x);
+	glTexCoord2f(0.0,1.0);
+	glVertex3f(0.0f, 10.0f, 10.0f + pos_x);
 	glEnd();
 	glPopMatrix();
 	
@@ -303,10 +288,14 @@ void draw(){
 	glBindTexture(GL_TEXTURE_2D, pared2);
 	glBegin(GL_POLYGON);
 	glNormal3f(0,0,1);
-	glTexCoord2f(0.0,0.0);	glVertex3f(0.0f, 0.0f, 0.0f);
-	glTexCoord2f(1.0,0.0);	glVertex3f(10.0f, 0.0f, 0.0f);
-	glTexCoord2f(1.0,1.0);	glVertex3f(10.0f, 10.0f, 0.0f );
-	glTexCoord2f(0.0,1.0);	glVertex3f(0.0f, 10.0f, 0.0f);
+	glTexCoord2f(0.0,0.0);
+	glVertex3f(0.0f, 0.0f, 0.0f + pos_x);
+	glTexCoord2f(1.0,0.0);
+	glVertex3f(10.0f, 0.0f, 0.0f + pos_x);
+	glTexCoord2f(1.0,1.0);
+	glVertex3f(10.0f, 10.0f, 0.0f + pos_x);
+	glTexCoord2f(0.0,1.0);
+	glVertex3f(0.0f, 10.0f, 0.0f + pos_x);
 	glEnd();
 	glPopMatrix();
 	
@@ -314,10 +303,14 @@ void draw(){
 	glBindTexture(GL_TEXTURE_2D, pared3);
 	glBegin(GL_POLYGON);
 	glNormal3f(0,0,1);
-	glTexCoord2f(0.0,0.0);	glVertex3f(0.0f, 0.0f, 0.0f);
-	glTexCoord2f(1.0,0.0);	glVertex3f(0.0f, 0.0f, 10.0f);
-	glTexCoord2f(1.0,1.0);	glVertex3f(0.0f, 10.0f, 10.0f );
-	glTexCoord2f(0.0,1.0);	glVertex3f(0.0f, 10.0f, 0.0f);
+	glTexCoord2f(0.0,0.0);
+	glVertex3f(0.0f, 0.0f, 0.0f + pos_x);
+	glTexCoord2f(1.0,0.0);
+	glVertex3f(0.0f, 0.0f, 10.0f + pos_x);
+	glTexCoord2f(1.0,1.0);
+	glVertex3f(0.0f, 10.0f, 10.0f + pos_x);
+	glTexCoord2f(0.0,1.0);
+	glVertex3f(0.0f, 10.0f, 0.0f + pos_x);
 	glEnd();
 	glPopMatrix();
 	
@@ -325,69 +318,87 @@ void draw(){
 	glBindTexture(GL_TEXTURE_2D, pared3);
 	glNormal3f(0,0,1);
 	glBegin(GL_POLYGON);
-		glTexCoord2f(0.0,0.0);	glVertex3f(10.0f, 0.0f, 0.0f);
-		glTexCoord2f(1.0,0.0);	glVertex3f(10.0f, 0.0f, 10.0f);
-		glTexCoord2f(1.0,1.0);	glVertex3f(10.0f, 10.0f, 10.0f );
-		glTexCoord2f(0.0,1.0);	glVertex3f(10.0f, 10.0f, 0.0f);
+		glTexCoord2f(0.0,0.0);
+		glVertex3f(10.0f, 0.0f, 0.0f + pos_x);
+		glTexCoord2f(1.0,0.0);
+		glVertex3f(10.0f, 0.0f, 10.0f + pos_x);
+		glTexCoord2f(1.0,1.0);
+		glVertex3f(10.0f, 10.0f, 10.0f + pos_x);
+		glTexCoord2f(0.0,1.0);
+		glVertex3f(10.0f, 10.0f, 0.0f + pos_x);
+		glEnd();
+		glPopMatrix();
+
+		//TECHOS
+
+		glPushMatrix();
+		glBindTexture(GL_TEXTURE_2D, techo);
+		glNormal3f(0, 0, 1);
+		glBegin(GL_TRIANGLES);
+		glTexCoord2f(0.0,0.0);
+		glVertex3f(10.0f, 10.0f, 0.0f + pos_x);
+		glTexCoord2f(4.0,0.0);
+		glVertex3f(10.0f, 15.0f, 5.0f + pos_x);
+		glTexCoord2f(0.0,4.0);
+		glVertex3f(10.0f, 10.0f, 10.0f + pos_x);
+		glEnd();
+		glPopMatrix();
+
+		glPushMatrix();
+		glBindTexture(GL_TEXTURE_2D, techo);
+		glNormal3f(0, 0, 1);
+		glBegin(GL_TRIANGLES);
+		glTexCoord2f(0.0,0.0);
+		glVertex3f(0.0f, 10.0f, 0.0f + pos_x);
+		glTexCoord2f(4.0,0.0);
+		glVertex3f(0.0f, 15.0f, 5.0f + pos_x);
+		glTexCoord2f(0.0,4.0);
+		glVertex3f(0.0f, 10.0f, 10.0f + pos_x);
+		glEnd();
+		glPopMatrix();
+
+		glPushMatrix();
+		glBindTexture(GL_TEXTURE_2D, tejado);
+		glNormal3f(1, 1, 0);
+		glBegin(GL_POLYGON);
+		glTexCoord2f(0.0,0.0);
+		glVertex3f(0.0f, 10.0f, 0.0f + pos_x);
+		glTexCoord2f(1.0,0.0);
+		glVertex3f(0.0f, 15.0f, 5.0f + pos_x);
+		glTexCoord2f(1.0,1.0);
+		glVertex3f(10.0f, 15.0f, 5.0f + pos_x);
+		glTexCoord2f(0.0,1.0);
+		glVertex3f(10.0f, 10.0f, 0.0f + pos_x);
+		glEnd();
+		glPopMatrix();
+
+		glPushMatrix();
+		glBindTexture(GL_TEXTURE_2D, tejado);
+		glNormal3f(1, 1, 0);
+		glBegin(GL_POLYGON);
+		glTexCoord2f(0.0,0.0);
+		glVertex3f(0.0f, 10.0f, 10.0f + pos_x);
+		glTexCoord2f(1.0,0.0);
+		glVertex3f(0.0f, 15.0f, 5.0f + pos_x);
+		glTexCoord2f(1.0,1.0);
+		glVertex3f(10.0f, 15.0f, 5.0f + pos_x);
+		glTexCoord2f(0.0,1.0);
+		glVertex3f(10.0f, 10.0f, 10.0f+pos_x);
 	glEnd();
 	glPopMatrix();
 	
-
-
-	//TECHOS
-
+	// ARBOL
 	glPushMatrix();
-	glBindTexture(GL_TEXTURE_2D, techo);
-	glNormal3f(0,0,1);
-	glBegin(GL_TRIANGLES);
-		glTexCoord2f(0.0,0.0);	glVertex3f(10.0f, 10.0f, 0.0f);
-		glTexCoord2f(4.0,0.0);	glVertex3f(10.0f, 15.0f, 5.0f);
-		glTexCoord2f(0.0,4.0);	glVertex3f(10.0f, 10.0f, 10.0f );
-	glEnd();
-	glPopMatrix();
-
-	glPushMatrix();
-	glBindTexture(GL_TEXTURE_2D, techo);
-	glNormal3f(0,0,1);
-	glBegin(GL_TRIANGLES);
-		glTexCoord2f(0.0,0.0);	glVertex3f(0.0f, 10.0f, 0.0f);
-		glTexCoord2f(4.0,0.0);	glVertex3f(0.0f, 15.0f, 5.0f);
-		glTexCoord2f(0.0,4.0);	glVertex3f(0.0f, 10.0f, 10.0f );
-	glEnd();
-	glPopMatrix();
-
-
-	glPushMatrix();
-	glBindTexture(GL_TEXTURE_2D, tejado);
-	glNormal3f(1,1,0);
-	glBegin(GL_POLYGON);
-		glTexCoord2f(0.0,0.0);	glVertex3f(0.0f, 10.0f, 0.0f);
-		glTexCoord2f(1.0,0.0);	glVertex3f(0.0f, 15.0f, 5.0f);
-		glTexCoord2f(1.0,1.0);	glVertex3f(10.0f, 15.0f, 5.0f );
-		glTexCoord2f(0.0,1.0);	glVertex3f(10.0f, 10.0f, 0.0f);
-	glEnd();
-	glPopMatrix();
-
-	glPushMatrix();
-	glBindTexture(GL_TEXTURE_2D, tejado);
-	glNormal3f(1,1,0);
-	glBegin(GL_POLYGON);
-		glTexCoord2f(0.0,0.0);	glVertex3f(0.0f, 10.0f, 10.0f);
-		glTexCoord2f(1.0,0.0);	glVertex3f(0.0f, 15.0f, 5.0f);
-		glTexCoord2f(1.0,1.0);	glVertex3f(10.0f, 15.0f, 5.0f );
-		glTexCoord2f(0.0,1.0);	glVertex3f(10.0f, 10.0f, 10.0f);
-	glEnd();
-	glPopMatrix();
-	
-		
-	// ARBOL 
+	glBindTexture(GL_TEXTURE_2D, pared1);
 	glTranslated(15.0f, 0.0f, 25.0f);
 	glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
 	gluCylinder(gluNewQuadric(),1.0f,1.0f,10.0f,32.0f,32.0f);
+
 	// HOJAS
+	glBindTexture(GL_TEXTURE_2D, suelo);
 	glTranslated(0.0f, 00.0f, 15.0f);
-	glutSolidSphere(5.0, 32.0f, 32.0f);
-	
+	glutSolidSphere(5.0, 8.0f, 8.0f);
+	glPopMatrix();
 	
 }
 
